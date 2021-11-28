6eb4388fc5a360bb9ab1d97de78bf451c76df9cb
ProdigySim <prodigysim@gmail.com>
2021-11-28 16:15:46 -0600

    Format

--- server/main.ts
+++ server/main.ts
@@ -3,24 +3,26 @@ import { renderServerApp } from "./ServerApp.tsx";
 
 const htmlHeaders = {
   "content-type": "text/html; charset=utf8",
-  "cache-control":
-    "no-cache, no-store, must-revalidate, max-age=0, s-maxage=0",
+  "cache-control": "no-cache, no-store, must-revalidate, max-age=0, s-maxage=0",
 };
 function handler(): Response {
-  console.log('request attempted');
+  console.log("request attempted");
   try {
-  return new Response(
-    getPageHtml(ReactDOMServer.renderToString(renderServerApp())),
-    {
-      headers: htmlHeaders,
-    },
-  );
-  } catch(e) {
+    return new Response(
+      getPageHtml(ReactDOMServer.renderToString(renderServerApp())),
+      {
+        headers: htmlHeaders,
+      },
+    );
+  } catch (e) {
     console.error(e);
-    return new Response(getPageHtml(`<h1>Internal Server Error</h1>`, 'Error'), {
-      headers: htmlHeaders,
-      status: 500,
-    });
+    return new Response(
+      getPageHtml(`<h1>Internal Server Error</h1>`, "Error"),
+      {
+        headers: htmlHeaders,
+        status: 500,
+      },
+    );
   }
 }
 
@@ -30,7 +32,7 @@ function getPageHtml(body: string, title?: string) {
     <head>
       <meta charset="UTF-8">
       <meta name="viewport" content="width=device-width, initial-scale=1.0">
-      <title>${title ?? 'Hola Warudo'}</title>
+      <title>${title ?? "Hola Warudo"}</title>
     </head>
     <body >
       ${body}
