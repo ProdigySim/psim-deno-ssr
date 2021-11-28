5c2a2e8d282b1e3c393f75842c6b16b1ccfc6f74
ProdigySim <prodigysim@gmail.com>
2021-11-28 16:04:40 -0600

    Error handling

--- server/main.ts
+++ server/main.ts
@@ -1,26 +1,36 @@
 import { ReactDOMServer, serve } from "./deps.ts";
 import { renderServerApp } from "./ServerApp.tsx";
 
+const htmlHeaders = {
+  "content-type": "text/html; charset=utf8",
+  "cache-control":
+    "no-cache, no-store, must-revalidate, max-age=0, s-maxage=0",
+};
 function handler(): Response {
+  console.log('request attempted');
+  try {
   return new Response(
     getPageHtml(ReactDOMServer.renderToString(renderServerApp())),
     {
-      headers: {
-        "content-type": "text/html; charset=utf8",
-        "cache-control":
-          "no-cache, no-store, must-revalidate, max-age=0, s-maxage=0",
-      },
+      headers: htmlHeaders,
     },
   );
+  } catch(e) {
+    console.error(e);
+    return new Response(getPageHtml(`<h1>Internal Server Error</h1>`, 'Error'), {
+      headers: htmlHeaders,
+      status: 500,
+    });
+  }
 }
 
-function getPageHtml(body: string) {
+function getPageHtml(body: string, title?: string) {
   return `<!DOCTYPE html>
     <html lang="en">
     <head>
       <meta charset="UTF-8">
       <meta name="viewport" content="width=device-width, initial-scale=1.0">
-      <title>Hola Warudo</title>
+      <title>${title ?? 'Hola Warudo'}</title>
     </head>
     <body >
       ${body}
